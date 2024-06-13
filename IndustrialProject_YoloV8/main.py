6ad7ad1cf06fd11afd51b0aa9aa8886cf1ac2ff4
jl2357 <jennylim366@gmail.com>
2024-06-14 00:11:22 +1200

    First draft of YOLOv8 on Pycharm
    Using YOLOv8 on pycharm as test run. Currently works, uses the pretrained model to detect objects in the frame. Quite laggy still

--- IndustrialProject_YoloV8/main.py
+++ IndustrialProject_YoloV8/main.py
@@ -1,1 +1,29 @@
-print("Hello World!")
+# Links
+# https://www.youtube.com/watch?v=uMzOcCNKr5A
+
+
+# imports
+from ultralytics import YOLO
+import cv2
+
+model = YOLO('yolov8n.pt')
+
+# Obtain video data
+video = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = video.read()
+    # Convert frame to grayscale
+    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    results = model.track(frame, persist=True)
+
+    frame_ = results[0].plot()
+    # Display the video
+    cv2.imshow("object", frame_)
+    # press q to exit program
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+video.release()
+cv2.destroyAllWindows()

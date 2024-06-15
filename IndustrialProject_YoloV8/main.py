6c4fd4556ae1caac9ee2e03ac535b39fe213796d
jl2357 <jennylim366@gmail.com>
2024-06-16 00:51:56 +1200

    added ncnn model
    trialling ncnn model. results: better performance than using without export to ncnn

--- IndustrialProject_YoloV8/main.py
+++ IndustrialProject_YoloV8/main.py
@@ -1,12 +1,18 @@
 # Links
 # https://www.youtube.com/watch?v=uMzOcCNKr5A
-
+# ncnn model: https://docs.ultralytics.com/integrations/ncnn/#usage
 
 # imports
 from ultralytics import YOLO
 import cv2
 
 model = YOLO('yolov8n.pt')
+
+# exporting to ncnn model
+model.export(format="ncnn")
+
+# loading the ncnn model
+ncnn_model = YOLO("./yolov8n_ncnn_model")
 
 # Obtain video data
 video = cv2.VideoCapture(0)
@@ -16,7 +22,7 @@ while True:
     # Convert frame to grayscale
     #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    results = model.track(frame, persist=True)
+    results = ncnn_model.track(frame, persist=True)
 
     frame_ = results[0].plot()
     # Display the video
